10f64494dd923bd484b35e4e625a1099b35e018a
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-26 11:17:57 -0300

    Rescritura de método que recebe requisição e envia respostas ao navegador.

--- src/main/java/br/com/github/lucasdevrj/platinado/servlet/Agendamento.java
+++ src/main/java/br/com/github/lucasdevrj/platinado/servlet/Agendamento.java
@@ -1,7 +1,23 @@
 package br.com.github.lucasdevrj.platinado.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class Agendamento extends HttpServlet {
+@WebServlet(urlPatterns = "/agendamento")
+public class Agendamento extends HttpServlet {//Classe para estender Servlet http
 
+	@Override//método que pega a requisição do usuário e devolve a resposta
+	protected void service(HttpServletRequest requisicao, HttpServletResponse resposta) throws IOException {
+		PrintWriter saida = resposta.getWriter();
+		saida.println("<html>");
+		saida.println("<body>");
+		saida.println("<p>Escrevendo no html com servlet</p>");
+		saida.println("</body>");
+		saida.println("</html>");
+	}
 }
