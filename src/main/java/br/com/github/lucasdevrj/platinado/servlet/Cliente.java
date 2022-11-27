f31a8c88dcfacc39175fd8cb63b9191c28ce9014
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-27 16:46:27 -0300

    Criação da classe Cliente.

---
+++ src/main/java/br/com/github/lucasdevrj/platinado/servlet/Cliente.java
@@ -0,0 +1,40 @@
+package br.com.github.lucasdevrj.platinado.servlet;
+
+public class Cliente {
+
+	private String nome;
+	private String sobrenome;
+	private String celular;
+	private String email;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
