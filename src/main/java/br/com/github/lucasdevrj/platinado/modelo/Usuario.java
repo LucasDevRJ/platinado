cdeddb101a382077b7482803024ede54a06230b7
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-12-07 11:37:51 -0300

    Adição da classe Usuario para login.

---
+++ src/main/java/br/com/github/lucasdevrj/platinado/modelo/Usuario.java
@@ -0,0 +1,35 @@
+package br.com.github.lucasdevrj.platinado.modelo;
+
+public class Usuario {
+
+    private String login;
+    private String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean ehIgual(String login, String senha) {
+        if(!this.login.equals(login)) {
+            return false;
+        }
+
+        if(!this.senha.equals(senha)) {
+            return false;
+        }
+
+        return true;
+    }
+}
